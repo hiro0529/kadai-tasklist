3c55814048e8f20e8d1fe140c0e972afa0c5d927
hirotaka.watanabe <kashimaantlers529@gmail.com>
2020-06-23 12:58:48 +0000

    change project

--- kadai-introduce.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $family_name = "渡部";
-    $first_name = "寛隆";
-    echo $family_name.$first_name."です。";
-    $age = "25";
-    echo $age."歳です。";
-
-?>
--- kadai-php-3/thinkable.php
+++
@@ -1,8 +0,0 @@
-<?php
-trait Thinkable {
-    public function think(){
-         print "私は".$this->hobby . "について考えています。".PHP_EOL;
-    }
-}
-
-?>
